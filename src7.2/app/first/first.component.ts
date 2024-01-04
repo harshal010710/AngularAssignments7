60371a9c3262d1042da62e2bf430d6370b942a77
Harshal Patil <107706576+harshal010710@users.noreply.github.com>
2024-01-04 23:33:54 +0530

    Add files via upload

---
+++ src7.2/app/first/first.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  standalone: true,
+  imports: [],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent 
+{
+  rtnStr() : string
+  {
+    return 'Marvellous';
+  }
+}
